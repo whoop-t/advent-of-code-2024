02db19e5e24af937f6e1fdd0411b1f193a998978
whoop-t <tommymallis@gmail.com>
2024-12-05 20:16:16 -0800

    day4 part2

--- day4/init.lua
+++ day4/init.lua
@@ -1,9 +1,9 @@
 local M = {}
-local answer = { "X", "M", "A", "S" }
+local answer_p1 = { "X", "M", "A", "S" }
 
 local function check(matrix, q)
   for i = 1, #q do
-    if not matrix[q[i][1]] or not matrix[q[i][1]][q[i][2]] or matrix[q[i][1]][q[i][2]] ~= answer[i + 1] then
+    if not matrix[q[i][1]] or not matrix[q[i][1]][q[i][2]] or matrix[q[i][1]][q[i][2]] ~= answer_p1[i + 1] then
       return false
     end
   end
@@ -29,7 +29,7 @@ M.part1 = function()
   for r = 1, #matrix do
     for c = 1, #matrix[r] do
       -- If we hit an X, then fan out(DFS or BFS) and see if we can find a word
-      if matrix[r][c] == answer[1] then
+      if matrix[r][c] == answer_p1[1] then
         -- Check in all directions
         local cur_total = 0
         -- Cardinal
@@ -38,10 +38,14 @@ M.part1 = function()
         cur_total = cur_total + (check(matrix, { { r - 1, c }, { r - 2, c }, { r - 3, c } }) and 1 or 0)
         cur_total = cur_total + (check(matrix, { { r + 1, c }, { r + 2, c }, { r + 3, c } }) and 1 or 0)
         --Diagonal
-        cur_total = cur_total + (check(matrix, { { r - 1, c - 1 }, { r - 2, c - 2 }, { r - 3, c - 3 } }) and 1 or 0)
-        cur_total = cur_total + (check(matrix, { { r - 1, c + 1 }, { r - 2, c + 2 }, { r - 3, c + 3 } }) and 1 or 0)
-        cur_total = cur_total + (check(matrix, { { r + 1, c - 1 }, { r + 2, c - 2 }, { r + 3, c - 3 } }) and 1 or 0)
-        cur_total = cur_total + (check(matrix, { { r + 1, c + 1 }, { r + 2, c + 2 }, { r + 3, c + 3 } }) and 1 or 0)
+        cur_total = cur_total
+            + (check(matrix, { { r - 1, c - 1 }, { r - 2, c - 2 }, { r - 3, c - 3 } }) and 1 or 0)
+        cur_total = cur_total
+            + (check(matrix, { { r - 1, c + 1 }, { r - 2, c + 2 }, { r - 3, c + 3 } }) and 1 or 0)
+        cur_total = cur_total
+            + (check(matrix, { { r + 1, c - 1 }, { r + 2, c - 2 }, { r + 3, c - 3 } }) and 1 or 0)
+        cur_total = cur_total
+            + (check(matrix, { { r + 1, c + 1 }, { r + 2, c + 2 }, { r + 3, c + 3 } }) and 1 or 0)
 
         total_found = total_found + cur_total
       end
@@ -51,6 +55,56 @@ M.part1 = function()
   print(total_found)
 end
 
-M.part2 = function() end
+M.part2 = function()
+  local matrix = {}
+  local total_found = 0
+  for line in io.lines("day4/part1-input.txt") do
+    -- Loop the line characters and put them in matrix
+    local array = {}
+    for i = 1, #line do
+      local c = line:sub(i, i)
+      table.insert(array, c)
+    end
+    table.insert(matrix, array)
+  end
+
+  -- Go through matrix and fan out to look for words
+  for r = 1, #matrix do
+    for c = 1, #matrix[r] do
+      if matrix[r][c] == "A" then
+        local l_to_r = false
+        local r_to_l = false
+
+        --top left and bottom right check
+        if
+            (matrix[r - 1] and matrix[r - 1][c - 1] and matrix[r + 1] and matrix[r + 1][c + 1])
+            and (
+              (matrix[r - 1][c - 1] == "M" and matrix[r + 1][c + 1] == "S")
+              or (matrix[r - 1][c - 1] == "S" and matrix[r + 1][c + 1] == "M")
+            )
+        then
+          l_to_r = true
+        end
+
+        --top right and bottom left check
+        if
+            (matrix[r - 1] and matrix[r - 1][c + 1] and matrix[r + 1] and matrix[r + 1][c - 1])
+            and (
+              (matrix[r - 1][c + 1] == "M" and matrix[r + 1][c - 1] == "S")
+              or (matrix[r - 1][c + 1] == "S" and matrix[r + 1][c - 1] == "M")
+            )
+        then
+          r_to_l = true
+        end
+
+        if l_to_r and r_to_l then
+          total_found = total_found + 1
+        end
+      end
+    end
+  end
+
+  print(total_found)
+end
 
 return M
--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@
 -- local day1 = require("day1")
 -- local day2 = require("day2")
 -- local day3 = require("day3")
-local day4 = require("day4")
+-- local day4 = require("day4")
 
 -- SOLVED
 -- day1.part1()
@@ -13,6 +13,6 @@ local day4 = require("day4")
 -- day2.part1()
 -- day2.part2()
 
--- NOT SOLVED
-day4.part1()
--- day3.part2()
+-- SOLVED
+-- day4.part1()
+-- day4.part2()
